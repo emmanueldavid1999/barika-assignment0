c02d1f5c5323a1bc226933e8bc7b26a458cc3b4a
Alfredjoshua12345 <132623343+Alfredjoshua12345@users.noreply.github.com>
2023-05-27 12:55:11 +0100

    Update Energy.java

--- Barka assignment/src/Energy.java
+++ Barka assignment/src/Energy.java
@@ -1,6 +1,6 @@
 public class Energy extends Physics{
 
-        public int acceleration(int initial_velocity, int final_velocity, int time){return initial_velocity - final_velocity /time;}
+        public int velocity(int displacement, int final_time){return int displacement /time;}
         
     }
 
